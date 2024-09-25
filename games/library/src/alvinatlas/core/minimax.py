4185c0ea4f0d70b001291751095f6915dc5a60c5
sanjayraojjn <rao_jjn@yahoo.com>
2024-09-25 20:48:37 +0530

    optimization: alpha beta pruning implemented. though not working good for game of NIM

--- games/library/src/alvinatlas/core/minimax.py
+++ games/library/src/alvinatlas/core/minimax.py
@@ -20,24 +20,42 @@ class Minimax:
             - if same states( (4, 0, 0) and (0, 0, 4) ) do not have the same hash 
             then the cache will not work
         """
+        alpha:int = self.min_score
+        beta:int = self.max_score
+
         if game_state.game_over:
             raise GameOver("Game is over")
         possible_next_states = set(game_state.possible_next_states)
 
         #print(f">>>>before {len(game_state.possible_next_states)} after {len(possible_next_states)}")
-        return max( (self.minimax_unordered(new_state, False), new_state ) \
+        return max( (self.minimax_unordered(new_state, False, alpha, beta), new_state ) \
                    for new_state in possible_next_states )
 
     @cache
-    def minimax_unordered(self, game_state: GameState, is_maximizing: bool)->GameState:
+    def minimax_unordered(self, game_state: GameState, is_maximizing: bool,
+                          alpha: int, beta: int)->GameState:
         """
         returns the next most probable winning gamestate but can be in different order
         """
         if game_state.game_over:
             return self.max_score if is_maximizing else self.min_score
+        
+        # scores = []
+        # for next_state in game_state.possible_next_states:
+        #     scores.append(score := self.minimax_unordered(next_state, not is_maximizing, alpha, beta))
+
+        #     if(is_maximizing):
+        #         alpha = max(alpha, score)
+        #     else:
+        #         beta = min(beta, score)
+        #     if beta < alpha:
+        #         #print(f"alpha {alpha} beta {beta} break {len(scores)}/{len(game_state.possible_next_states)}>>>>>>>>>>>>")
+        #         break
+
+        # return (max if is_maximizing else min) (scores)
 
         return (max if is_maximizing else min) \
-                ( self.minimax_unordered(next_state, not is_maximizing) for next_state in game_state.possible_next_states )
+               ( self.minimax_unordered(next_state, not is_maximizing, alpha, beta) for next_state in game_state.possible_next_states )
 
         
 
